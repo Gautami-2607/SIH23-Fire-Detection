2c14eac5bf2fba64c296525c6b80d3b6b1f0e06f
Gautami Mamidala <gautamisaisatya@gmail.com>
2023-12-19 07:48:46 +0000

    images are being displayed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI, Request, File, UploadFile, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
